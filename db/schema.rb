4b00909c684b0bcac2d412b5892b3b7f231b9236
che30 <che.blancoo58@gmail.com>
2023-11-05 20:07:02 +0100

    Fix decimal data type

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_11_05_162611) do
+ActiveRecord::Schema[7.0].define(version: 2023_11_05_185117) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -62,6 +62,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_11_05_162611) do
     t.bigint "customer_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name"
     t.index ["customer_id"], name: "index_goods_on_customer_id"
   end
 
