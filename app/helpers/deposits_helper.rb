48b909ffdbe2257a8f9a284e30bea2217b646b96
che30 <che.blancoo58@gmail.com>
2023-11-23 14:17:45 +0100

    Refactor deposit show view

--- app/helpers/deposits_helper.rb
+++ app/helpers/deposits_helper.rb
@@ -1,2 +1,9 @@
 module DepositsHelper
+    def deposit(status)
+        if status 
+            content_tag(:td, "received")
+        else
+            content_tag(:td, "not received")
+        end
+    end
 end
