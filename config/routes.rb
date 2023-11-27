88b5126c0f775b3d915026efd1c1663cbb699b7f
che30 <che.blancoo58@gmail.com>
2023-11-27 05:17:16 +0100

    Add secret key base

--- config/routes.rb
+++ config/routes.rb
@@ -22,7 +22,6 @@ Rails.application.routes.draw do
   resources :receipts do
     get 'download', on: :member
   end
-  # post 'login', to: "cherubim_users#login"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
